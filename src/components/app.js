c0c66bd6c0a6047e195d92eec7403c17339ca652
elcocinero1982 <cortezluisjose@gmail.com>
2020-03-31 18:55:34 -0700

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>DevCamp React Starter</h1>
+        <h3>Maldito animal mugroso</h3>
+        <h2>Perro maldito app</h2>
+      </div>
+    );
+  }
+}
